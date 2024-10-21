29f05a7ec9874ae849336a0129afd44fa04b11cb
elangdor <179868961+elangdor@users.noreply.github.com>
2024-10-21 22:45:08 +0200

    Did exercise 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) { 
+    return "ERROR";
+    } else {
+        let resultString = "";
+        for (i = 0; i < num; i++) {
+            resultString = resultString + string;
+        }
+        
+        return resultString;
+    }
 };
 
+console.log(repeatString(2, "hej"));
 // Do not edit below this line
 module.exports = repeatString;

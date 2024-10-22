961109aaac42e91a27c64a930c1a7913680fb92e
elangdor <179868961+elangdor@users.noreply.github.com>
2024-10-22 10:38:23 +0200

    Finished lesson 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(inputText) {
+    let inputArray = Array.from(inputText);
+    let outputArray = [];
+    for (let i = inputArray.length; i > 0; i--) {
+        outputArray.push(inputArray.pop());
+    }
+    let outputText = outputArray.join("");
+    return outputText;
 };
+
+console.log(reverseString('hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    //need to remove multiple as well
+    /*if (array.indexOf(remove)) {
+        while(array.indexOf(remove) !== -1) {
+            let removeIndex = array.indexOf(remove);
+            array.splice(removeIndex, 1);
+        }
+    }*/
+    for (let i = 1; i < arguments.length; i++){
+        if (array.indexOf(arguments[i]) !== -1) {
+            while (array.indexOf(arguments[i]) !== -1 ) {
+                let removeIndex2 = array.indexOf(arguments[i])
+                array.splice(removeIndex2, 1);
+            }
+        }
+
+    }
+    return array;
 };
+
+
+console.log(removeFromArray([1,2,3,4,5,6,7,1,1,1,2,2], 2,1));
 
 // Do not edit below this line
 module.exports = removeFromArray;
